e9fb6b932602397415a378d4d0d3d7a7670cc34a
Tomoki Sato <t_sato@agenda.co.jp>
2018-10-05 14:52:36 +0900

    実際のマークシートで検証

--- marksheet_reader.py
+++ marksheet_reader.py
@@ -14,55 +14,59 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--imgdir", type = str, default = None, help = "スキャンした画像のあるディレクトリーです。")
 parser.add_argument("--verbose", action = "store_true", help = "個々の読み取り結果を表示します。")
+parser.add_argument("--threshold", type = float, default = 0.5, help = "マーカー点の認識閾値です。")
 FLAGS = parser.parse_args()
 
 
 ##### マーカー設定
-marker_dpi = 112			# マーカーサイズ
-scan_dpi = 400				# スキャン画像の解像度
-marker_threshold = 0.5		# マーカー点の認識閾値
+marker_dpi = 112                    # マーカーサイズ
+scan_dpi = 200                      # スキャン画像の解像度
+marker_threshold = FLAGS.threshold  # マーカー点の認識閾値
 
 # マーカー画像をグレースケールで読み込む
 marker = cv2.imread("marker.jpg", 0)
 
 # マーカーのサイズを保管
-marker_width, marker_height = (64, 64)
-# marker_width, marker_height = marker.shape[::-1]
-# print("W =", marker_width, ", H =", marker_height)
+marker_src_width, marker_src_height = marker.shape[::-1]
+# print("W =", marker_src_width, ", H =", marker_src_height)
 
 # 解像度に合わせてマーカーのサイズを変更
-marker = cv2.resize(marker, (marker_width, marker_height))
-# marker = cv2.resize(marker, (int(marker_height * scan_dpi / marker_dpi), int(marker_width * scan_dpi / marker_dpi)))
-# print("マーカー認識サイズ:", marker.shape[::-1])
+marker_dest_width, marker_dest_height = (64, 64)
+# marker_dest_width, marker_dest_height = (int(marker_src_height * scan_dpi / marker_dpi), int(marker_src_width * scan_dpi / marker_dpi))
+marker = cv2.resize(marker, (marker_dest_width, marker_dest_height))
+print("マーカー認識サイズ:", marker.shape[::-1])
 
 
 ##### マークシート設定
-n_col = 5							# マークシートの列数＝一行あたりのマーク数
-n_row = 11							# マークシートの行数（≠ 項目数）
-n_question = math.ceil(n_row / 2.0)	# マークシートの項目数（偶数行は空白にする＝行間を空けてマークしやすくする）
-margin_top = 5						# 上部余白の行数
-margin_bottom = 3					# 下部余白の行数
-total_row = n_row + margin_top + margin_bottom		# 余白を含めた行数
-size = 100							# １行１列あたりのサイズ
-gray_threshold = 50					# 二値化の閾値
-result_threshold_minrate = 0.1		# 塗りつぶしていると判断する最小割合の閾値
-result_threshold_rate = 3.0			# 塗りつぶしていると決定する中央値からの倍率
+n_col = 6                           # マークシートの列数＝一行あたりのマーク数
+n_row = 35                          # マークシートの行数（≠ 項目数）
+# n_question = math.ceil(n_row / 2.0)	# マークシートの項目数（偶数行は空白にする＝行間を空けてマークしやすくする）
+n_question = n_row                  # マークシートの項目数（偶数行は空白にする＝行間を空けてマークしやすくする）
+margin_top = 1                      # 上部余白の行数
+margin_bottom = 1                   # 下部余白の行数
+total_row = n_row + margin_top + margin_bottom   # 余白を含めた行数
+size = 100                          # １行１列あたりのサイズ
+gray_threshold = 120                # 二値化の閾値
+result_threshold_minrate = 0.1      # 塗りつぶしていると判断する最小割合の閾値
+result_threshold_rate = 4.0         # 塗りつぶしていると決定する中央値からの倍率
 
 
 # 集計設定
-summary_dir = "summary"				# 書き出し先のディレクトリー名
+summary_dir = "summary"             # 書き出し先のディレクトリー名
 
 
 ##### マークシートを読み込み、認識できる状態に整形します。
 # 認識に失敗した場合は None を返します。
 def loadMarkSheet(filename):
 	global marker, n_col, total_row, margin_top, margin_bottom, size, gray_threshold
+	basename = os.path.basename(filename)
 
 	# スキャン画像の取り込み
 	image = cv2.imread(filename, 0)
 
 	# スキャン画像の中からマーカーを抽出
 	res = cv2.matchTemplate(image, marker, cv2.TM_CCOEFF_NORMED)
+	# print(res)
 
 	# 類似度の閾値以上の座標を取り出す
 	loc = np.where(res >= marker_threshold)
@@ -75,32 +79,36 @@ def loadMarkSheet(filename):
 	mark_area = {}
 	mark_area["top_x"] = min(loc[1])
 	mark_area["top_y"] = min(loc[0])
-	mark_area["bottom_x"] = max(loc[1] + marker_width)
+	mark_area["bottom_x"] = max(loc[1] + marker_dest_width)
 	mark_area["bottom_y"] = max(loc[0])
+	print(mark_area)
 
 	image = image[mark_area["top_y"] : mark_area["bottom_y"], mark_area["top_x"] : mark_area["bottom_x"]]
 
 	# 切り出した領域を検証用に書き出し
-	# cv2.imwrite(os.path.join(path, "scan_cropped.jpg"), image)
+	cv2.imwrite(os.path.join(basename + "-scan_cropped.jpg"), image)
 
 	# 列数、行数ベースでキリのいいサイズにリサイズ
 	image = cv2.resize(image, (n_col * size, total_row * size))
 
 	# 画像に軽くブラーをかけて２値化し、白黒反転させる（塗りつぶした部分が白く浮き上がる）
 	image = cv2.GaussianBlur(image, (5, 5), 0)
-	res, image = cv2.threshold(image, gray_threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	# res, image = cv2.threshold(image, gray_threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	res, image = cv2.threshold(image, gray_threshold, 255, cv2.THRESH_BINARY)
 	image = 255 - image
 
 	# 認識可能な状態の画像を検証用に書き出し
-	# cv2.imwrite(os.path.join(path, "scan_inverted.jpg"), image)
+	# cv2.imwrite(os.path.join(basename + "-scan_inverted.jpg"), image)
 
 	return image
 
 
+
 ##### 読み込まれたマークシートをもとに、塗りつぶされた項目の列番号を認識して配列で返します。
 # ここに渡す画像は二値化されており、かつ１行と１列でサイズが等しいことが前提となります。
-def recognizeMarkSheet(image):
+def recognizeMarkSheet(image, filename):
 	global n_col, total_row, margin_top, margin_bottom, size
+	basename = os.path.basename(filename)
 
 	results = []
 
@@ -110,12 +118,13 @@ def recognizeMarkSheet(image):
 
 	# 行ごとに走査する
 	for row in range(margin_top, total_row - margin_bottom):
-		if row % 2 == 0:
+		# if row % 2 == 0:
 			# 偶数行は空白にする（行間を空けてマークしやすくする）ため飛ばす
-			continue
+			# continue
 
 		# 処理する行を切り出して 0-1 標準化
 		row_image = image[row * size : (row + 1) * size, ] / 255.0
+		cv2.imwrite(os.path.join(basename + "-row" + str(row) + ".jpg"), image[row * size : (row + 1) * size, ])
 		area_sum = []	# ここに合計値を入れる
 
 		# 列ごとに走査する
@@ -133,12 +142,16 @@ def recognizeMarkSheet(image):
 
 		if max >= result_threshold_minrate:
 			# 最大値が閾値を上回っており、かつ、塗りつぶされた部分の面積が中央値の３倍以上かどうかで結果を論理値判定する
-			results.append(area_sum > med * result_threshold_rate)
+			result = area_sum > med * result_threshold_rate
+			result = np.asarray([1 if x == True else 0 for x in result])
+			# print(result)
+			results.append(result)
 		else:
 			# 最大値が閾値を下回っている場合、無効票とする
 			results.append([])
 
 	return results
+
 
 
 ##### メインルーチン
@@ -199,18 +212,19 @@ if __name__ == '__main__':
 			continue
 		else:
 			# マーク読み取り実行
-			results = recognizeMarkSheet(image)
+			results = recognizeMarkSheet(image, filename)
 
 		# 読み取り結果を集計
 		for row, result in enumerate(results):
-			data = np.where(result == True)[0] + 1
+			data = np.where(result == 1)[0] + 1
+			data = data.astype(np.uint8)
 
 			if len(data) == 1:
 				# 単一回答
 				if FLAGS.verbose:
-					print("Q-%02d. " % (row + 1) + str(data))
+					print("Q-%02d. " % (row + 1) + str(data[0]))
 
-				data_sum.iat[row, int(data[0]) - 1] = data_sum.iat[row, int(data[0]) - 1] + 1
+				data_sum.iat[row, data[0]] = str(int(data_sum.iat[row, data[0]]) + 1)
 
 			elif len(data) > 1:
 				# 複数回答
@@ -221,7 +235,7 @@ if __name__ == '__main__':
 					pd.Series(
 						[
 							file,
-							row + 1,
+							int(row + 1),
 							str(data),
 						],
 						index = multi_ans.columns
@@ -238,7 +252,7 @@ if __name__ == '__main__':
 					pd.Series(
 						[
 							file,
-							row + 1,
+							int(row + 1),
 						],
 						index = no_ans.columns
 					),
