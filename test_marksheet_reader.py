678a9eaf2715bd148f8fa6a1f5f7ac0c87e69aa6
Tomoki Sato <t_sato@agenda.co.jp>
2019-09-11 18:07:10 +0900

    .gitignore 反映

---
+++ test_marksheet_reader.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+###############################################################################
+#    単体テストケース
+###############################################################################
+from unittest import TestCase
+import marksheet_reader
+
+
+class TestMarksheetReader(TestCase):
+    pass
